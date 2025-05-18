f1034cb47d239cb8a56ef1269bab0e11e5de041b
yazidears <uwuzide@gmail.com>
2025-05-18 19:54:11 +0200

    Created

---
+++ StreetPass/StreetPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetPassApp.swift
+//  StreetPass
+//
+//  Created by Yazide Arsalan on 17/5/25.
+//
+
+import SwiftUI
+
+@main
+struct StreetPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
